f7e9a084cdb26690f476ca9dc88c58c29e5a0b8f
YeshayahuBennun <ybennun@yahoo.com>
2021-10-12 19:16:06 +0300

    commit#08 - Fitness App - Overflow Menu With Navigation

--- app/src/main/java/com/ybennun/fitnessapp/StartFragment.kt
+++ app/src/main/java/com/ybennun/fitnessapp/StartFragment.kt
@@ -1,13 +1,12 @@
 package com.ybennun.fitnessapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class StartFragment : Fragment() {
 
@@ -25,6 +24,19 @@ class StartFragment : Fragment() {
         goButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_exerciseFragment)
         )
+
+        setHasOptionsMenu(true)
         return v
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())||
+        super.onOptionsItemSelected(item)
     }
 }
